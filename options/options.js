21d01c698bc524f2abafacf3465e8d1a18e98949
Crowdbotics <engineering@crowdbotics.com>
2023-03-30 06:16:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ravinder-stage-apri-73232",
+  url: "https://ravinder-stage-apri-73232.botics.co",
+  api: "https://ravinder-stage-apri-73232.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
